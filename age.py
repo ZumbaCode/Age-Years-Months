4350ac7443f3afbac46e50c54b6ea5929c7c3038
ZumbaCode <100531041+ZumbaCode@users.noreply.github.com>
2022-02-28 16:25:08 -0500

    Add files via upload

---
+++ age.py
@@ -0,0 +1,22 @@
+age = int(input("Enter age: "))
+
+years_Remaining = 90 - age
+
+days_Remaining = years_Remaining * 365
+
+weeks_remaining = years_Remaining * 52
+
+months_Remaining = years_Remaining * 12
+
+print(
+    "You have",
+    weeks_remaining,
+    "weeks_remaining",
+    "You have",
+    "Months",
+    months_Remaining,
+    "You have",
+    "Years remaining",
+    years_Remaining,
+    "Years remaining",
+)
